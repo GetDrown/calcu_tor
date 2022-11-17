4cd007bea97fd2be258ab2a5d654c1fe0f49dca5
Kyle <kyledominicp@gmail.com>
2022-11-17 21:46:02 +0800

    temporary remove cont

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,10 @@ export class AppComponent {
   public appPages = [
     { title: 'Home', url: '/folder/Home', icon: 'home' },
     { title: 'Schedule', url: '/folder/Schedule', icon: 'calendar-number' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+   // { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
+   // { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
+   // { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
+   // { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
