e0c8fbeacf9d2ccbc9530ca2b56bc3d30018e51d
Kyle <kyledominicp@gmail.com>
2022-11-19 22:45:40 +0800

    wrongfully deleted the folder

--- src/app/pages/loader/loader.module.ts
+++ src/app/pages/loader/loader.module.ts
@@ -3,9 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
-
 import { LoaderPageRoutingModule } from './loader-routing.module';
-
 import { LoaderPage } from './loader.page';
 
 @NgModule({
